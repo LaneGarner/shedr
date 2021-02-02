9c1ac240ce67c923ed9f2d8233deee53286bedd4
Lane Garner <b.lane.garner@gmail.com>
2021-02-02 10:39:55 -0600

    push

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { HomeIcon } from "../icons/HomeIcon";
+import { UserIcon } from "../icons/UserIcon";
 
 export const Header = () => {
+    const styles = {
+        header: {
+            backgroundColor: "#333",
+            color: "#fff"
+        }
+    };
     return (
-        <header style={style.header}>
+        <header style={styles.header}>
+            <HomeIcon />
+            <UserIcon />
             <h1>shedr</h1>
         </header>
     )
