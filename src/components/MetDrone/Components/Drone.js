7c635ba0b272720bc50af478b52665bcf999919d
Lane Garner <b.lane.garner@gmail.com>
2021-02-03 20:42:12 -0600

    test on mobile

--- src/components/MetDrone/Components/Drone.js
+++ src/components/MetDrone/Components/Drone.js
@@ -4,65 +4,133 @@ import * as Tone from 'tone'
 import StartAudioContext from 'startaudiocontext'
 import VolumeDown from '@material-ui/icons/VolumeDown';
 import VolumeUp from '@material-ui/icons/VolumeUp';
-import { Chord } from "@tonaljs/tonal";
+import { Chord, ChordType } from "@tonaljs/tonal";
 
+const synth = new Tone.PolySynth({
+    oscillator: {
+        type: "sine"
+    },
+    envelope: {
+        attack: 2,
+        decay: 2,
+        sustain: 2,
+        release: 5,
+    },
+});
+
+let chord;
 
 export const Drone = () => {
-    const [droning, setDroning] = useState(false);
-    const [volume, setVolume] = useState(50);
+    const [droning, setDroning] = useState(false)
+    const [volume, setVolume] = useState(-10)
+    const [root, setRoot] = useState("C")
+    const [chordType, setChordType] = useState("minor ninth")
+    
+    let myChord = Chord.getChord(chordType, `${root}4`, `${root}5`);
+    const chordTypes = ChordType.names()
 
-    const chord = ["C2", "C3", "C4", "Eb4", "G4", "Bb4", "D4"]
 
-    const synth = new Tone.PolySynth({
-        oscillator: {
-            type: "sine"
-        },
-        envelope: {
-            attack: 4,
-            decay: 2,
-            sustain: 1,
-            release: 1.5,
-        },
-    });
+    // let chord = Chord.getChord("min9", "C4").notes;
+    // let chord
+
+    useEffect(() => {
+        // const myChord = Chord.get(root + chordType);
+            myChord = Chord.getChord(chordType, `${root}4`);
+            // const myChord = Chord.getChord(chordType, `${root}4`, "C5");
+
+        chord = myChord.notes
+    }, [root, chordType])
+
+    // const myRoot = `"${root}4"`
+    // const myChord = Chord.get(root + chordType);
+    // const myChord = Chord.getChord(chordType, `${root}4`, "C5");
+    // const chord = myChord.notes
+    
+    const test = () => {
+        const chordTypes = ChordType.names()
+        // chordTypes.map(chord => console.log(chord))
+        console.log(myChord)
+        // console.log(`"${root}4"`)
+    }
+    
     const filter = new Tone.Filter(2000, "lowpass")
     const verb = new Tone.Reverb(1)
     const wide = new Tone.StereoWidener()
+    // const vlm = volume
+    // const vol = new Tone.Volume({volume})
+    // const vol = new Tone.Volume(-12).toDestination();
 
+    // synth.chain( filter, verb, wide, vol, Tone.Destination)
     synth.chain( filter, verb, wide, Tone.Destination)
 
     // const handleDroneStart = () => {
     //     StartAudioContext(Tone.context)
-    //     setPlaying(true)
+    //     setDroning(true)
+    //     synth.triggerAttack(chord)
     // }
 
-    const stopDrone = () => {
-        synth.releaseAll()
-        setDroning(false)
-        // console.log("stop")
-    }
+    // const stopDrone = () => {
+    //     synth.triggerRelease(chord)
+    //     // synth.releaseAll()
+    //     setDroning(false)
+    //     // console.log("stop")
+    // }
 
     useEffect(() => {
-        droning && synth.triggerAttack(chord);
+        droning ? synth.triggerAttack(chord) : synth.triggerRelease(chord);
     }, [droning])
     
 
-    // useEffect(() => {
-    //     // synth.volume = volume
-    // }, [volume])
+    useEffect(() => {
+        // stopDrone()
+        synth.volume.value = volume
+        console.log(synth.volume.value)
+    }, [volume])
 
     return (
         <div className="Drone">
+            <button onClick={test}>Test</button>
             <h1 style={{color: "white"}}>drone</h1>
-            {!droning ? <button onClick={() => setDroning(!droning)}>Start</button> : <button onClick={stopDrone}>Stop</button>}
-            {/* <button onClick={() => setDroning(!droning)}>
-            {droning ? "Stop" : "Start"}
-            </button> */}
-            {/* <button onClick={stopDrone}>stop</button> */}
+            <input 
+                value={root} 
+                onChange={(e) => setRoot(e.target.value)}
+                type="text" 
+                placeholder="root"/>
+            <select value={chordType} onChange={(e) => setChordType(e.target.value)} name="selectDroneRoot" id="selectDroneRoot">
+                {chordTypes.map((chord, id) => <option key={id} value={chord}>{chord}</option>)}
+                {/* <optgroup label="Triads">
+                    <option value="min">min</option>
+                    <option value="major">maj</option>
+                    <option value="dim">dim</option>
+                    <option value="aug">aug</option>{}
+                </optgroup>
+                <optgroup label="7th">
+                    <option value="maj7">maj7</option>
+                    <option value="7">7</option>
+                    <option value="min7">min7</option>
+                    <option value="min7b5">min7b5</option>
+                    <option value="dim7">dim7</option>
+                    <option value="min7#5">min7#5</option>
+                </optgroup>
+                <optgroup label="9th">
+                    <option value="maj9">maj9</option>
+                    <option value="9">9</option>
+                    <option value="minor9">min9</option>
+                </optgroup> */}
+            </select>
+
+            {/* {!droning ? <button onClick={() => setDroning(!droning)}>Start</button> : <button onClick={stopDrone}>Stop</button>} */}
+            <button onClick={() => setDroning(!droning)}>
+                {!droning ? "Start" : "Stop"}
+            </button>
             <div>
                 <VolumeDown />
                 <input 
                     type="range"
-                    volume={volume}
+                    min={-100}
+                    max={0}
+                    step="0.1"
+                    value={volume}
                     onChange={(e) => setVolume(e.target.value)}
                 />
                 <VolumeUp />
