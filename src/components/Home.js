b5e57d663570065d607b567675e1a21c7fb2653c
Lane Garner <b.lane.garner@gmail.com>
2021-03-16 15:53:15 -0500

    landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,9 @@ import { StoreContext } from '../Store'
 
 import { Logo } from "../icons/Logo"
 import { FlatGuitarist } from "../images/FlatGuitarist"
+import { FlatFemaleGuitarist } from "../images/FlatFemaleGuitarist"
+import { FlatDrummer } from "../images/FlatDrummer"
+import { FlatPianist } from "../images/FlatPianist"
 
 import image from "../flat-gtrist-preview.png"
 
@@ -20,14 +23,39 @@ export const Home = () => {
 
     return (
         <div className="home-container">
-            {/* <Logo height="20em"/> */}
-            <div>
-                <FlatGuitarist />
+            <div className="home-container-section">
+                <div>
+                    <FlatGuitarist />
+                </div>
+                <div>
+                    <h1>Welcome to Shedr</h1>
+                    <p>Shedr is a musician's practice toolkit designed to sit on your music stand while practicing. It is optimized for desktop, tablet, and mobile so you can practice efficiently  wherever you are.</p>
+                    <Link to="/dashboard" className="button"> Get Started</Link>
+                </div>
             </div>
-            <div>
-                <h1>Welcome to Shedr</h1>
-                <p>Shedr is a musican's practice toolkit designed to sit on your music stand while you practice. It is optimized for desktop, tablet, and mobile so you can practice effeciently wherever you are.</p>
-                <Link to="/dashboard" className="button"> Get Started</Link>
+            <div className="home-container-section">
+                <div>
+                    <h2>All the tools you need in one place</h2>
+                    <ul>
+                        <li><span>Practice Log-</span> Time and track of your practice sessions</li>
+                        <li><span>Repertoire List-</span> Build and review your repertoire</li>
+                        <li><span>Practice Stats-</span> Stay motivated by analyzing your progress</li>
+                        <li><span>MetDrone-</span> Practice with an accurate metronome and a harmonic drone</li>
+                        <li><span>Recordings-</span> Record your practice sessions to revisit later</li>
+                        <li><span>Tuner-</span> Make sure your instrument is in tune for each practice session</li>
+                    </ul>
+                </div>
+                <div>
+                    <FlatDrummer/>
+                </div>
+            </div>
+            <div className="home-container-section">
+                <div>
+                    <FlatFemaleGuitarist/>
+                </div>
+                <div>
+                    <FlatPianist/>
+                </div>
             </div>
             {/* <img style={{width: "40em"}} src={image}/> */}
             {/* {activeSession ?  */}
