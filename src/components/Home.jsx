a66d0fa532925d011a3442800ecc39907e0d65e4
Lane Garner <b.lane.garner@gmail.com>
2021-04-12 13:16:35 -0500

    adjusted header and home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,7 +25,7 @@ export const Home = () => {
                 </div>
                 <div>
                     <h1>Welcome to Shedr</h1>
-                    <p>Shedr is a musician's practice toolkit designed to sit on your music stand while practicing. It is optimized for desktop, tablet, and mobile so you can practice efficiently  wherever you are.</p>
+                    <p>Shedr is a musician's practice toolkit designed for desktop, tablet, and mobile so you can practice efficiently wherever you are.</p>
                     <Link to="/dashboard" className="button"> Get Started</Link>
                 </div>
             </div>
@@ -33,11 +33,14 @@ export const Home = () => {
                 <div>
                     <h2>Everything you need in one place</h2>
                     <ul>
+                        {/* <div>
+                            <FlatFemaleGuitarist/>
+                        </div> */}
                         <li><span>Practice Log</span> - Track and time your practice sessions</li>
                         <li><span>Repertoire List</span> - Build and review your repertoire</li>
                         <li><span>Practice Stats</span> - Stay motivated by analyzing your progress</li>
                         <li><span>MetDrone</span> - Practice with an accurate metronome and a harmonic drone</li>
-                        <li><span>Recordings</span> - Record your practice sessions to revisit later</li>
+                        {/* <li><span>Recordings</span> - Record your practice sessions to revisit later</li> */}
                         <li><span>Tuner</span> - Make sure your instrument is in tune for each practice session</li>
                     </ul>
                 </div>
